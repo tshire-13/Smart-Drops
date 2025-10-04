ec42a8f1a42c458e5f864eca858d26592b542b7a
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-10-04 13:27:56 +0200

    removed whitespace

--- Backend/controller/user.js
+++ Backend/controller/user.js
@@ -5,7 +5,6 @@ const submitForm  = async(req,res)=> {
     const now = new Date();
     const timestamp = now.toISOString();
 
-
     const {name, email, number, location_description, 
         severity, description, image_url, latitude, longitude, 
     } = req.body
