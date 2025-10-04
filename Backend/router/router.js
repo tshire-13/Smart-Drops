2ccbe2ddee0e9e9c265be7b680bca7ea60fc42b1
Letlhogonolo <164743557+whoIsImai@users.noreply.github.com>
2025-10-04 21:27:25 +0200

    get a single piece of data

--- Backend/router/router.js
+++ Backend/router/router.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { submitForm, upload, getData } from '../controller/user.js'
+import { submitForm, upload, getData, getSingleData } from '../controller/user.js'
 
 router.post('/submit', upload.single("image"),submitForm)
 router.get('/data', getData)
+router.get('/data/:id', getSingleData)
 
 
 export default router
